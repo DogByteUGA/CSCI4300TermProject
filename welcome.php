35d0734df11c74bc4dc61f689ec4eec2b38253d1
chongt <tiffue@uga.edu>
2015-04-17 01:45:44 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,4 +1,5 @@
 <?php 
+
 ?>
 
 <html>
@@ -7,104 +8,108 @@
        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-         	<style>
+        
+        <style>
         * {
   			font-size: 12pt;
   			font-family: "Verdana", "Geneva", sans-serif;
-			}
+		}
 
-			h1{
+		h1{
   			margin-top: 0em;
-			}
+		}
 
-			body {
+		body {
   			background-color: white;
-			}
+		}
 
-			#bannerarea{
-				margin-top: 10px;
-				margin-left: 10px;
-			}
+		#bannerarea{
+			margin-top: 10px;
+			margin-left: 10px;
+		}
   			
-			h2 {
-			  	color: white;
-  				font-weight: bold;
-			}
+		h2 {
+		  	color: white;
+  			font-weight: bold;
+		}
 
-			h3{
-				margin-left: 10px;
-				font-weight: bold;
-			}
+		h3{
+			margin-left: 10px;
+			font-weight: bold;
+		}
 
-			#signUp{
-  				background-color: #FFFFFF;
-			}
+		#signUp{
+  			background-color: #FFFFFF;
+		}
 
-			input[type="button"] {
-	  			background-color: #FFFFFF;
-  				font-size: 120%;
-			}
+		input[type="button"] {
+	  		background-color: #FFFFFF;
+  			font-size: 120%;
+		}
 
-			#login {
-  				background-image: url("https://41.media.tumblr.com/1cfb06a166f9021fb1eb8640e6611bf8/tumblr_nlx2p33nuR1tkwpyuo2_r1_540.jpg");
-  				padding-bottom: 100px;
-  				padding-right: 700px;
-  				padding-top: 100px;
-  				padding-left: 100px;
-			}
+		#login {
+  			background-image: url("https://41.media.tumblr.com/1cfb06a166f9021fb1eb8640e6611bf8/tumblr_nlx2p33nuR1tkwpyuo2_r1_540.jpg");
+  			padding-bottom: 100px;
+  			padding-right: 700px;
+  			padding-top: 100px;
+  			padding-left: 100px;
+		}
 
-			button[type="button"]{
-				background-color: #470000;
-				border-color: black;
-			}
+		button[type="button"]{
+			background-color: #470000;
+			border-color: black;
+		}
 
-			button[type="button"]:hover{
-				background-color: #470000;
-				border-color: black;
-			}
+		button[type="button"]:hover{
+			background-color: #470000;
+			border-color: black;
+		}
 
-			input[type="submit"] {
-  				background-color: #FFFFFF;
-  				font-size: 120%;
-			}
+		input[type="submit"] {
+			background-color: #FFFFFF;
+			font-size: 120%;
+		}
 
-			.checkbox {
-  				color: white;
-  				font-size: 120%;
-			}
+		.checkbox {
+			color: white;
+  			font-size: 120%;
+		}
 
 		</style>
 	
 	</head>
+	
 	<body>
 		<div id="bannerarea">
 			<img src="https://41.media.tumblr.com/671ba642b48ed5a3f92438cc796417ac/tumblr_nlx2p33nuR1tkwpyuo1_r1_400.png" alt="banner logo"> 
 			<br>
-		
-
-		<h1>Get started on your projects today!</h1>
-
-		<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  			Sign Up
-		</button>
-
-		<br>
-		<br>
+			<h1>Get started on your projects today!</h1>
+			<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal"> Sign Up 
+			</button>
+			<br>
+			<br>	
 		</div>
-
+		
 		<div id="login">
 			<h2>Already have an account? Welcome back!</h2>
-			<form action = "project-search.php" method ="get">
-			<input type="text" name="userLogin" class="textfield" placeholder="Username">
-			<br><br>
-			<input type="text" name="userPassword" class="textfield" placeholder="Password">
-			<br>
-  		   <div class="checkbox">
-    		   <label><input type="checkbox"> Remember me</label>
-  		   </div>
-			<input type="submit" name="submit" class="submit" value="Sign In">
+			<form action = "./login-check.php" method ="post" accept-charset="utf-8">
+				<input type="text" name="username" class="textfield" placeholder="Username" maxlength="20">
+				<br>
+				<br>
+				<input type="text" name="password" class="textfield" placeholder="Password" maxlength="20">
+				<br>
+  		   		
+  		   		<div class="checkbox">
+    		   		<label><input type="checkbox"> Remember me</label>
+  		   		</div>
+
+				<input type="submit" name="submit" class="submit" value="Sign In">
+		
 			</form>
+		
 		</div>
+
+
 
  		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   		<div class="modal-dialog">
