d84151c373588ade106510401a728e76a6da45dd
chongt <tiffue@uga.edu>
2015-04-18 00:52:07 -0400

    Create template.php

---
+++ template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>{name}'s Profile</title>
+	<style>
+        * {
+  			font-size: 12pt;
+  			font-family: "Verdana", "Geneva", sans-serif;
+		}
+		h3{
+			margin-left: 10px;
+		}
+	</style>
+</head>
+
+<body>
+	<img src="https://41.media.tumblr.com/671ba642b48ed5a3f92438cc796417ac/tumblr_nlx2p33nuR1tkwpyuo1_r1_400.png" alt="banner logo"> 
+	<p>{image}</p>
+	<ul>Name: {name}</ul>
+	<ul>Email: {email} </ul>
+	<ul>Phone Number: {number} </ul>
+	<ul>Address: {address} </ul>
+	<ul>Major: {major} </ul>
+
+	<h3>Current Projects:</h3>
+
+</body>
+</html>
