be719ca749a58b81a08b6e9f778c7584c2d7c119
chongt <tiffue@uga.edu>
2015-04-20 14:34:24 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -81,10 +81,6 @@
 	</head>
 	
 	<body>
-
-
-
-
 		<div id="bannerarea">
 			<img src="https://41.media.tumblr.com/671ba642b48ed5a3f92438cc796417ac/tumblr_nlx2p33nuR1tkwpyuo1_r1_400.png" alt="banner logo"> 
 			<br>
@@ -202,11 +198,13 @@
 <?php // php stuff for making user profile
 //Template for basic page
 $template = <<<EOD
-	<style>
+<?php 
+   include '../user-header.php';
+?>
 <!DOCTYPE html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>{name}'s Profile</title>
+	<title>{username}'s Profile</title>
 	<style>
         * {
   			font-size: 12pt;
@@ -215,38 +213,58 @@ $template = <<<EOD
 		h3{
 			margin-left: 10px;
 		}
-		#description-bg{
+		#bg{
 			background-image: url("https://41.media.tumblr.com/1cfb06a166f9021fb1eb8640e6611bf8/tumblr_nlx2p33nuR1tkwpyuo2_r1_540.jpg");
-  			padding-bottom: 100px;
-  			padding-top: 100px;
+  			margin-top: 300px;
+  			padding-bottom: 300px;
 		}
 		#description{
-			background-color: white;
 			font-size: 12pt;
-  			font-family: "Verdana", "Geneva", sans-serif;
+			font-family: "Verdana", "Geneva", sans-serif;
+			position: relative;
+			float: left;
+			padding-right: 200px;
+			margin-top: 50px;
 		}
-
+		#projects{
+			position: relative;
+			float: left;
+			margin-top: 25px;
+		}
+		#avatar{
+			position: relative;
+			float: left;
+		}
+		input[type="submit"] {
+			background-color: #FFFFFF;
+			font-size: 120%;
+		}
 	</style>
 </head>
 
 <body>
-	<img src="https://41.media.tumblr.com/671ba642b48ed5a3f92438cc796417ac/tumblr_nlx2p33nuR1tkwpyuo1_r1_400.png" alt="banner logo"> 
-	<br>
-	<br>
-	<div id="description-bg">
-		<div id="description">
-			<br>
-			<p>{image}</p>
-			<ul>Name: {name}</ul>
-			<ul>Email: {email} </ul>
-			<ul>Phone Number: {number} </ul>
-			<ul>Address: {address} </ul>
-			<ul>Major: {major} </ul>
-
-			<h3>Current Projects:</h3>
-			<br>
-		</div>
+	<div id="avatar">
+		<ul><img src="https://41.media.tumblr.com/9f93f36af014846dc6046e2c2a920473/tumblr_nlx2p33nuR1tkwpyuo3_r1_250.jpg"></ul>
 	</div>
+	<div id="description">
+		<ul><b>Name:</b> {name}</ul>
+		<ul><b>Email:</b> {email} </ul>
+		<ul><b>Phone Number:</b> {number} </ul>
+		<ul><b>Address:</b> {address} </ul>
+		<ul><b>Major:</b> {major} </ul>
+	</div>
+	<div id="projects">
+		<h3>Current Projects:</h3>
+			<ul>
+				<form action="../project-create.php">
+					<input type="submit" value="Create a new project!">
+					</input>
+				</form>
+			</ul>
+	</div>
+	<div id="bg">
+	</div>
+
 </body>
 </html>
 
