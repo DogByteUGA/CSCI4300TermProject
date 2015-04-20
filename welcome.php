c6c00d75be92fdeae8827e2eadc925aac789b122
chongt <tiffue@uga.edu>
2015-04-19 21:53:39 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -155,10 +155,11 @@
 <?php // php stuff for making user profile
 //Template for basic page
 $template = <<<EOD
+	<style>
 <!DOCTYPE html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>{username}</title>
+	<title>{name}'s Profile</title>
 	<style>
         * {
   			font-size: 12pt;
@@ -167,20 +168,41 @@ $template = <<<EOD
 		h3{
 			margin-left: 10px;
 		}
+		#description-bg{
+			background-image: url("https://41.media.tumblr.com/1cfb06a166f9021fb1eb8640e6611bf8/tumblr_nlx2p33nuR1tkwpyuo2_r1_540.jpg");
+  			padding-bottom: 100px;
+  			padding-top: 100px;
+		}
+		#description{
+			background-color: white;
+			font-size: 12pt;
+  			font-family: "Verdana", "Geneva", sans-serif;
+		}
+
 	</style>
 </head>
+
 <body>
 	<img src="https://41.media.tumblr.com/671ba642b48ed5a3f92438cc796417ac/tumblr_nlx2p33nuR1tkwpyuo1_r1_400.png" alt="banner logo"> 
-	<p>{username}</p>
-	<p>{image}</p>
-	<ul>Name: {name}</ul>
-	<ul>Email: {email} </ul>
-	<ul>Phone Number: {number} </ul>
-	<ul>Address: {address} </ul>
-	<ul>Major: {major} </ul>
-	<h3>Current Projects:</h3>
+	<br>
+	<br>
+	<div id="description-bg">
+		<div id="description">
+			<br>
+			<p>{image}</p>
+			<ul>Name: {name}</ul>
+			<ul>Email: {email} </ul>
+			<ul>Phone Number: {number} </ul>
+			<ul>Address: {address} </ul>
+			<ul>Major: {major} </ul>
+
+			<h3>Current Projects:</h3>
+			<br>
+		</div>
+	</div>
 </body>
 </html>
+
 EOD;
 
 //handle the posted form
@@ -221,7 +243,7 @@ if(isset($_POST['username'])){
 		<?php //notify if successful making user profile
 			if(isset($notice)){
 		?>
-			<h4>Succesfully registered! Please sign into your account to get started.</h4>
+			<h4>Successfully registered! Please sign into your account to get started.</h4>
 		<?php
 		} 
 		?>
